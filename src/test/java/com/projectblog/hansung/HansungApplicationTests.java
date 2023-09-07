3d28306696cb0b3a93b9b73eeb44555fd4d35940
Koul <dongik0122@gmail.com>
2023-09-07 19:19:15 +0900

    초기설정

---
+++ src/test/java/com/projectblog/hansung/HansungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectblog.hansung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HansungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
